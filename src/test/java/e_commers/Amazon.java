7c6a38660543712ae8b86f21b448f9b193663c0c
saurabh <pardeshisaurabh61@gmail.com>
2024-02-17 17:17:05 +0530

    [SP] This is my last push

--- src/test/java/e_commers/Amazon.java
+++ src/test/java/e_commers/Amazon.java
@@ -11,6 +11,7 @@ public class Amazon {
 	  System.setProperty("webdriver.chrome.driver","D:\\Chrome downlode file\\Chrome driver Selenium\\chromedriver-win64\\chromedriver.exe");
 	  Reporter.log("Opening Amazon",true);
 	  WebDriver driver =new ChromeDriver ();
+	  driver.manage().window().maximize();
 	  driver.get("https://www.amazon.in/");
 	  driver.close();
   }
