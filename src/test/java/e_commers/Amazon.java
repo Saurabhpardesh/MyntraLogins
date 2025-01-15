df17123a73fe91f4b25c40e1d9825c9633b3cf5b
Saurabhpardesh <pardeshisaurabh61@gmail.com>
2025-01-15 18:16:21 +0530

    [15/01/2025] push

--- src/test/java/e_commers/Amazon.java
+++ src/test/java/e_commers/Amazon.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 
 public class Amazon {
   @Test
-  public void AmezonTest()  {
+  public void AmezonTest()   {
 	  System.setProperty("webdriver.chrome.driver","D:\\Downlods\\Chrome downlode file\\Chrome driver Selenium\\chromedriver-win64\\chromedriver.exe");
 	  Reporter.log("Opening Amazon",true);
 	  WebDriver Driver =new ChromeDriver();
